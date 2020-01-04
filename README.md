7d3a4294821ecfa15f2b32ac83b6d65524df17b4
abhiram <cmabhiram@gmail.com>
2020-01-04 17:27:26 +0530

    authentication added

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-This is to save blogs. Data fields are : Id,title, body, author_name, email The saved blogs can be viewed and deleted
+This is to save blogs. 
+
+Data fields are : Id,title, body, author_name, email 
+
+The saved blogs can be viewed and deleted
 
 You can check by using curl commands and this is provided in curl.txt file
+
+1) curl -v --user abc:123 http://localhost:8000
+   
+Above mentioned is a curl command to the homepage. "--user" describes the username and password
+   
+More commands given in curl.txt file
--- main.go
+++ main.go
@@ -20,13 +20,15 @@ type Blog struct {
     Email string `json:email` 
 }
 
+var database *sql.DB
+var err error
+
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Println("Endpoint Hit: homePage")
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	}
 
 func createBlog(w http.ResponseWriter, r *http.Request){
-	database,_ := sql.Open("sqlite3", "./blog.db")
 	reqBody,_ :=ioutil.ReadAll(r.Body)
 	var blog Blog	
 	json.Unmarshal([]byte(reqBody), &blog)
@@ -36,7 +38,6 @@ func createBlog(w http.ResponseWriter, r *http.Request){
 }
 
 func allBlogs(w http.ResponseWriter, r *http.Request){
-	database,_ := sql.Open("sqlite3", "./blog.db")
 	fmt.Println("Endpoint Hit: AllBlogs")
 	rows,_ := database.Query("select Id,title, body, author_name, email from blog where length(title)>1")
 	defer rows.Close()
@@ -48,7 +49,6 @@ func allBlogs(w http.ResponseWriter, r *http.Request){
 }
 
 func getBlog(w http.ResponseWriter, r *http.Request){
-	database,_ := sql.Open("sqlite3", "./blog.db")
 	params := mux.Vars(r)
 	rows,_ := database.Query("select Id,title, body, author_name, email from blog where Id=?",params["id"])
 	defer rows.Close()
@@ -61,25 +61,43 @@ func getBlog(w http.ResponseWriter, r *http.Request){
 }
 
 func deletePost(w http.ResponseWriter, r *http.Request) {
-	database,_ := sql.Open("sqlite3", "./blog.db")
   	params := mux.Vars(r)
   	statement,_ := database.Prepare("DELETE FROM blog WHERE Id = ?")
 	statement.Exec(params["id"])
   	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
 }
 
+func BasicAuthMiddleware(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		fmt.Println("username: ", user)
+		fmt.Println("password: ", pass)
+		if !ok || !checkUsernameAndPassword(user, pass) {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Please enter your username and password for this site"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorised.\n"))
+			return
+		}
+		handler(w, r)
+	}
+}
+
+func checkUsernameAndPassword(username, password string) bool {
+	return username == "abc" && password == "123"
+}
+
 func handleRequest(){
 	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.HandleFunc("/",homePage)
-	myRouter.HandleFunc("/all",allBlogs)
-	myRouter.HandleFunc("/new",createBlog).Methods("POST")
-	myRouter.HandleFunc("/new/{id}",deletePost).Methods("DELETE")
-	myRouter.HandleFunc("/new/{id}",getBlog)
+	myRouter.HandleFunc("/",BasicAuthMiddleware(http.HandlerFunc(homePage)))
+	myRouter.HandleFunc("/all",BasicAuthMiddleware(http.HandlerFunc(allBlogs)))
+	myRouter.HandleFunc("/new",BasicAuthMiddleware(http.HandlerFunc(createBlog))).Methods("POST")
+	myRouter.HandleFunc("/new/{id}",BasicAuthMiddleware(http.HandlerFunc(deletePost))).Methods("DELETE")
+	myRouter.HandleFunc("/new/{id}",BasicAuthMiddleware(http.HandlerFunc(getBlog)))
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
 
 func main(){
-database,_ := sql.Open("sqlite3", "./blog.db")
+database,_ = sql.Open("sqlite3", "./blog.db")
 statement,_ := database.Prepare("CREATE TABLE IF NOT EXISTS blog(Id INTEGER PRIMARY KEY,title TEXT, body TEXT, author_name TEXT, email TEXT)")	
 statement.Exec()	
 handleRequest()	
